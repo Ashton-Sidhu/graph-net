d93562774766a708458056cee65c4f8f8ae20615
Ashton Sidhu <sidhu@pop-os.localdomain>
2020-01-27 02:46:33 -0500

    updated install files

--- install_logstash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get install apt-transport-https -y
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get update && sudo apt-get install logstash
